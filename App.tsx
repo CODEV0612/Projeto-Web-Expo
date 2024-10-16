4c081fdc7197bdd43dd840e1a39082f46e24c51c
CODEV0612 <vitor.m.santos7@ba.estudante.senai.br>
2024-10-16 08:55:20 -0300

    leves alterações de importação no App.tsx

--- App.tsx
+++ App.tsx
@@ -1,22 +1,26 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'; // Use isso
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import LoginScreen from './src/LoginScreen';
 import CadastroScreen from './src/CadastroScreen';
+import MapScreen from './src/MapScreen';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator(); // Atualize aqui
 
 function App(): React.JSX.Element {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Cadastro">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Cadastro" component={CadastroScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Cadastro">
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Cadastro" component={CadastroScreen} />
+          <Stack.Screen name="Mapa" component={MapScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
 
 export default App;
-
